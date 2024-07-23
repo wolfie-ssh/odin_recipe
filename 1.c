2ba70e4e61a1c928a18e4608647e29a2072e39b9
wolfie-ssh <tilakg0p1605@gmail.com>
2024-07-23 18:49:44 +0530

    Addedd Some Lines

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+int speed=20;
+int time=7;
+int distance = speed*time;
+printf("Distance: %d\n" distance);
+return 0;
+}
--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-  printf("Hello, World\n"); // "\n" is used to take the cursor to the next line
-  return 0;
-}
