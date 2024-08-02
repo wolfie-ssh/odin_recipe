7f9cc89661d9b0f1e46dfbb142cb4d69c0f0dad3
wolfie-ssh <tilakg0p1605@gmail.com>
2024-08-02 20:19:14 +0530

    Added Some Programs

--- arrays.c
+++ arrays.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
-int main(void) {
+int main(void) 
+
+    {
+    int grade[5];
+    grade[0]=92;
+    grade[1]=85;
+    grade[2]=72;
+    grade[3]=73;
+    grade[4]=95;
+
+    grade[2]=grade[2]+18;
     
-}
+    for(int i=0;i<5;i++)
+        printf("Grade[%d]=%d\n", i, grade[i]);
+
+    int total=0;
+    for (int i=0; i<5;i++)
+        total+=grade[i];
+    printf("average of the grades is %d\n", total/5);
+     }
