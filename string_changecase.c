cd8acc1206078830e6a8fabda3a0060f514bce52
wolfie-ssh <tilakg0p1605@gmail.com>
2024-08-05 19:00:36 +0530

    Added Some Programs

--- string_changecase.c
+++ string_changecase.c
@@ -2,20 +2,18 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(void) { 
-    char s[]="This is the way";
+int main(void)
+{
+    char s[]="This Is The WAY";
     int length=strlen(s);
     for(int i=0; i<length; i++)
         {
             printf("%c ", s[i]);
             if(isupper(s[i]))
-                printf("-Upper Case\n");
+                s[i]= tolower(s[i]);
             else if (islower(s[i]))
-                printf("-Lower\n");
-            else printf("\n");
+                s[i]=toupper(s[i]);
         }
+    printf("s: %s \n", s);
     return 0;
-
-
-
 }
--- string_cmp.c
+++ string_cmp.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main(void) {
     char s1[]="This is a string";
-    char s2[]="This is A string";
+    char s2[]="This is a string";
 
     if (strcmp(s1,s2)==0) // string compare (strcmp(1,2)) returns 0 if condition is true
         printf("Equal Strings\n");
