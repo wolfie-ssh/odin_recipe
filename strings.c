bdca2f3c2656d1634f7c8c79f266f3132c210f60
wolfie-ssh <tilakg0p1605@gmail.com>
2024-08-05 12:55:53 +0530

    Added Some Programs

--- strings.c
+++ strings.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main (void) {
-    char s1[]="This is a String";
+    char s1[]="This is a String.";
 
-    for(int i=0;i<16;i++)
+    for(int i=0;i<17;i++)
         printf("s1[%d] = %c\n", i, s1[i]);
 
 }
