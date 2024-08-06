49a871caef712f6471b3960277fec6a0ec50b935
wolfie-ssh <tilakg0p1605@gmail.com>
2024-08-06 08:48:47 +0530

    Added Some Programs

---
+++ func_findmax.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+ int find_max(int a,int b)
+ {
+    if (a>b) return a;
+    else return b;
+ }
+
+ int main(void)
+ {
+    int max=find_max(10,25);
+    printf("Maximum Value is: %d\n", max);
+    return 0;
+ }
