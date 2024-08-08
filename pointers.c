711cd57bcf84b27f8ed779250d0c69573c92ce39
wolfie-ssh <tilakg0p1605@gmail.com>
2024-08-08 12:21:39 +0530

    Added Some Programs

--- pointers.c
+++ pointers.c
@@ -6,13 +6,12 @@ int main (void)
     int b=10;
     int *p;
     p = &b;
-    a=a+*p;
-    
-    /* printf("%d\n", *p);
-    printf("p: %p\n", p);
+    printf(" p: %p\n", p);
     printf("&b: %p\n", &b);
     printf("&a: %p\n", &a);
-    printf("a: %d\n", a);
-    return 0; */
-
+    p=&a;
+    printf(" p: %p\n", p);
+    *p=*p+18;
+    printf(" a: %d\n", a);
+    return 0;
 }
