19f7ff7e43d5aed4f39a57febc7dfe89ba52d6b8
wolfie-ssh <tilakg0p1605@gmail.com>
2024-08-08 20:14:17 +0530

    Added Some Programs

--- typedef_struct.c
+++ typedef_struct.c
@@ -15,14 +15,14 @@ int main(void)
         printf("(%d, %d)\n", p1.x, p1.y);
 
     struct Point p2;
-        p2.x=5;
-        p2.y=3;
+        p2.x=10;
+        p2.y=6;
         printf("(%d, %d)\n", p2.x, p2.y);
 
     struct Point p3;
-        p3.x=5;
-        p3.y=3;
-        printf("(%d, %d)\n", p2.x, p2.y);
+        p3.x=20;
+        p3.y=12;
+        printf("(%d, %d)\n", p3.x, p3.y);
 
     return 0;
 }
